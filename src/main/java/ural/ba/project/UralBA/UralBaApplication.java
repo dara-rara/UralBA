686b7d888b192958fa4f194ed01cd0bdc5fa0d90
Daria Glebova <glebova555dasha@gmail.com>
2025-11-25 20:25:54 +0500

    Initial commit

---
+++ src/main/java/ural/ba/project/UralBA/UralBaApplication.java
@@ -0,0 +1,13 @@
+package ural.ba.project.UralBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UralBaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UralBaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ural/ba/project/UralBA/UralBaApplicationTests.java
@@ -0,0 +1,13 @@
+package ural.ba.project.UralBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UralBaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
