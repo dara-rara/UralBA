235711f4b46473f3f539f8d8d615ec179b9179e8
Daria Glebova <glebova555dasha@gmail.com>
2026-01-16 23:03:00 +0500

    Доработка cors

--- src/main/java/ural/ba/project/UralBA/config/CorsConfig.java
+++ src/main/java/ural/ba/project/UralBA/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ural.ba.project.UralBA.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,16 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  *
  * @author Daria
  */
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("https://dianasuf-community-analysts-aecb.twc1.net")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://dianasuf-community-analysts-aecb.twc1.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("*")
+                .exposedHeaders("Authorization", "X-Refresh-Token", "Set-Cookie")
                 .allowCredentials(true)
                 .maxAge(3600L);
     }
